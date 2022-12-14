215b96fb2890d7990d31314027d4c9da063802d3
rensilva5 <renzoasilva@gmail.com>
2022-12-14 13:46:57 -0500

    Creating new react-native, rating App

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.text}>Hello, new App wiht react-native</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.        
+        </Text>
+        <Text>
+          This is my new react-native-app! Happy to share it.
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "grey",
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  text: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "#ffff99",
+    padding: 20,
   },
 });
