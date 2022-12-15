7915342f0e80703f74297eb8d9d5b89a3230b46d
rensilva5 <renzoasilva@gmail.com>
2022-12-14 23:41:55 -0500

    Adding text inputs

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
 
   const [name, setName] = useState('Paul')
-  const [person, setPerson] = useState({name: 'Karl', age: 42})
-
-  const clickHandler = () => {
-    setName('Lee')
-    setPerson({name: 'Larry', age: 43})
-  }
+  const [age, setAge] = useState('31')
 
   return (
     <View style={styles.container}>
-        <Text style={styles.text}>My name is {name}</Text>
-        <Text>My name is {person.name} and I am {person.age} old</Text>
+      <Text>Enter Name</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder='e.g. Jhon Smith'
+        onChangeText={(val) => setName(val)}
+        />
+      <Text>Enter age</Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.input}
+        placeholder='e.g. 18'
+        onChangeText={(val) => setAge(val)}
+        />
+
+        <Text>name: {name} age: {age}</Text>
         <View style={styles.buttonContainer}>
-          <Button title='Update state' onPress={clickHandler} />
         </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -31,22 +38,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  header: {
-    backgroundColor: "grey",
-    paddingLeft: 40,
-    paddingRight: 40,
-    paddingTop: 20,
-    paddingBottom: 20,
-  },
-  text: {
-    fontWeight: "bold",
-  },
   body: {
     backgroundColor: "#ffff99",
     padding: 20,
   },
-  buttonContainer: {
-    marginTop: 20,
-    backgroundColor: 'orange'
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+    backgroundColor: 'grey'
   }
 });
