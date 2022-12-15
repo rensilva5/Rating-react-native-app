1ed6031f6709bc08a96d42ad13c1427c2049396d
rensilva5 <renzoasilva@gmail.com>
2022-12-15 14:23:36 -0500

    List and Scrollview option

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-
-  const [name, setName] = useState('Paul')
-  const [age, setAge] = useState('31')
+  const [people, setPeople] = useState([
+    { name: "Paul", key: "1" },
+    { name: "Mario", key: "2" },
+    { name: "Ricky", key: "3" },
+    { name: "Ramon", key: "4" },
+    { name: "Carlos", key: "5" },
+    { name: "Ryan", key: "6" },
+    { name: "Jhon", key: "7" },
+    { name: "Kelly", key: "8" },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Enter Name</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder='e.g. Jhon Smith'
-        onChangeText={(val) => setName(val)}
-        />
-      <Text>Enter age</Text>
-      <TextInput
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder='e.g. 18'
-        onChangeText={(val) => setAge(val)}
-        />
-
-        <Text>name: {name} age: {age}</Text>
-        <View style={styles.buttonContainer}>
-        </View>
+      <ScrollView>
+        {people.map(item => (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          ))} 
+      </ScrollView>
     </View>
   );
 }
@@ -35,19 +31,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  body: {
-    backgroundColor: "#ffff99",
-    padding: 20,
+  // body: {
+  //   backgroundColor: "#ffff99",
+  //   padding: 20,
+  // },
+  // input: {
+  //   borderWidth: 1,
+  //   borderColor: '#777',
+  //   padding: 8,
+  //   margin: 10,
+  //   width: 200,
+  //   backgroundColor: 'grey'
+  // }
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-    backgroundColor: 'grey'
-  }
 });
