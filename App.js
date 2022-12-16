62711b04be52c312c2b07b28e250840a1cbda273
rensilva5 <renzoasilva@gmail.com>
2022-12-15 19:29:13 -0500

    Creating the app first part, adding a new component and nested it in my app component

--- App.js
+++ App.js
@@ -1,55 +1,36 @@
 // import { StatusBar } from "expo-status-bar";
+import Header from "./components/header";
 import { useState } from "react";
 import {
   FlatList,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from "react-native";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: "Paul", id: "1" },
-    { name: "Mario", id: "2" },
-    { name: "Ricky", id: "3" },
-    { name: "Ramon", id: "4" },
-    { name: "Carlos", id: "5" },
-    { name: "Ryan", id: "6" },
-    { name: "Jhon", id: "7" },
-    { name: "Kelly", id: "8" },
-    { name: "Jenny", id: "9" },
-    { name: "Mark", id: "10" },
-  ]);
-
-  const pressHandler = (id) => {
-    // console.log(id)
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id) //return a beelean, this case is false, so it filters out of the new array.
-    })
-  }
+  const [todos, setTodos] = useState([
+    { text: 'buy shampoo', key: '1'},
+    { text: 'buy soap', key: '2'},
+    { text: 'pay water bill', key: '3'},
+  ])
 
   return (
     <View style={styles.container}>
-      {/* flatList component  less code */}
+      <Header />
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
 
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      {/* <ScrollView>
-        {people.map(item => (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          ))} 
-      </ScrollView> */}
+      </View>
+      
     </View>
   );
 }
@@ -59,10 +40,79 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "lightblue",
     paddingTop: 40,
-    paddingHorizontal: 20,
+    paddingHorizontal: 20,    
+  },
+  content: {
+    padding: 40,
+
+  },
+  list: {
+    marginTop: 20,
+  }
+});
+
+
+// import { useState } from "react";
+// import {
+//   FlatList,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   View,
+// } from "react-native";
+
+// export default function App() {
+//   const [people, setPeople] = useState([
+//     { name: "Paul", id: "1" },
+//     { name: "Mario", id: "2" },
+//     { name: "Ricky", id: "3" },
+//     { name: "Ramon", id: "4" },
+//     { name: "Carlos", id: "5" },
+//     { name: "Ryan", id: "6" },
+//     { name: "Mark", id: "7" },
+//   ]);
+
+  // const pressHandler = (id) => {
+    // console.log(id)
+    // setPeople((prevPeople) => {
+      // return prevPeople.filter(person => person.id != id) //return a beelean, this case is false, so it filters out of the new array.
+    // })
+  // }
+
+  // return (
+    // <View style={styles.container}>
+      /* flatList component  less code */
+
+      // <FlatList
+        // numColumns={2}
+        // keyExtractor={(item) => item.id}
+        // data={people}
+        // renderItem={({ item }) => (
+          // <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            /* <Text style={styles.item}>{item.name}</Text> */
+          /* </TouchableOpacity> */
+        // )}
+      // />
+      /* <ScrollView>
+        {people.map(item => (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          ))} 
+      </ScrollView> */
+    // </View>
+  // );
+// }
+
+// const styles = StyleSheet.create({
+  // container: {
+    // flex: 1,
+    // backgroundColor: "lightblue",
+    // paddingTop: 40,
+    // paddingHorizontal: 20,
     // alignItems: "center",
     // justifyContent: "center",
-  },
+  // },
   // body: {
   //   backgroundColor: "#ffff99",
   //   padding: 20,
@@ -75,12 +125,12 @@ const styles = StyleSheet.create({
   //   width: 200,
   //   backgroundColor: 'grey'
   // }
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
-  },
-});
+//   item: {
+//     marginTop: 24,
+//     padding: 30,
+//     backgroundColor: "pink",
+//     fontSize: 24,
+//     marginHorizontal: 10,
+//     marginTop: 24,
+//   },
+// })
---
+++ components/header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My todos</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 30,
+    backgroundColor: "coral",
+  },
+  title: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
+
+export default Header;
